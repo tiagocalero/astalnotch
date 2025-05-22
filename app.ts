18528ff60fc032d9c57e46aa87304a201be29f4d
tiagocalero <tiago@tcalero.com>
2025-05-22 12:50:07 -0300

    first push

---
+++ app.ts
@@ -0,0 +1,18 @@
+import { App } from "astal/gtk3";
+// import style from "./style/main.scss";
+// import Hyprnotch from "./src/view/Hyprnotch";
+// import WorkspacesBar from "./src/view/workspaces/WorkspacesBar";
+// import Bar from "./src/view/bar/Bar";
+App.start({
+  // css: style,
+  // main() {
+  //   App.get_monitors().map((monitor) => {
+  //     Hyprnotch(monitor);
+  //     Bar(monitor);
+  //   });
+  // },
+
+    main() {
+
+    }
+});
